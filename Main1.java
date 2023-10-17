b3225afbccb4e747d5a97dca674dfab63a6c3216
zlx <test@test.com>
2023-10-17 16:37:58 +0800

    main 1

--- Main1.java
+++
@@ -1,8 +0,0 @@
-package cn.edu.zju;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-        System.out.println("Hello Main!");
-    }
-}

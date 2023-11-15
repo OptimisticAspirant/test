671bbbe61f3c4b2a9a9a14a8a4e7534b87b91f8f
zlxzgtc <44161097+zlxzgtc@users.noreply.github.com>
2023-11-15 14:27:39 +0800

    test pr 7 (#13)
    * C0
    
    * C1
    
    * C2
    
    * C2
    
    * ln
    
    * zlx
    
    * ln
    
    * zlx
    
    ---------
    
    Co-authored-by: ln <1602113077@qq.com>

--- Main.java
+++ Main.java
@@ -2,6 +2,7 @@ package cn.edu.zju;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello ln");
+        System.out.println("Hello zlx");
     }
 }

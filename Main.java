5b33769f80f92b927a93fd60fa6b6b97c883d1ed
zlx <2780311834@qq.com>
2023-11-15 18:23:08 +0800

    zlx2

--- Main.java
+++ Main.java
@@ -3,6 +3,6 @@ package cn.edu.zju;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello ln2");
-        System.out.println("Hello zlx1");
+        System.out.println("Hello zlx2");
     }
 }

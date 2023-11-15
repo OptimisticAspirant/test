2394f3c8e1b7fa87299c2cc80326a3c950a3c2ed
ln <1602113077@qq.com>
2023-11-15 13:48:35 +0800

    C2

--- Main.java
+++ Main.java
@@ -3,7 +3,5 @@ package cn.edu.zju;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        System.out.println("Hello World!");
-        System.out.println("Hello World!");
     }
 }

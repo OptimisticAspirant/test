ee91dc931bfd80cfc7e24e32f241360ee18f433c
ln <1602113077@qq.com>
2023-11-15 18:22:48 +0800

    ln2

--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@ package cn.edu.zju;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello ln1");
+        System.out.println("Hello ln2");
         System.out.println("Hello zlx1");
     }
 }
